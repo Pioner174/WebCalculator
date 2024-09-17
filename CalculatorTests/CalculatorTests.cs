1c50e9e863b57b54eeb0f5b372516bbdb77196c9
Roman <romashka951@gmail.com>
2024-09-17 23:21:16 +0500

    Добавлены юнит тесты калькулятора

---
+++ CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,66 @@
+﻿using WebCalculator;
+
+namespace CalculatorTests
+{
+    public class CalculatorTests
+    {
+        private ICalculator _calculator; //ссылка на сервис калькулятора
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new ICalculator(); 
+        }
+
+        [Test]
+        public void Test_Addition()
+        {
+            string expression = "2+3";
+            var result = _calculator.Calculate(expression);
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Test_Subtraction()
+        {
+            string expression = "5-2";
+            var result = _calculator.Calculate(expression);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Test_Multiplication()
+        {
+            string expression = "4*3";
+            var result = _calculator.Calculate(expression);
+            Assert.That(result, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void Test_Division()
+        {
+            string expression = "8/2";
+            var result = _calculator.Calculate(expression);
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Test_Parentheses()
+        {
+            string expression = "(2+3)*2";
+            var result = _calculator.Calculate(expression);
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Test_InvalidExpression()
+        {
+            string expression = "2++3"; // Некорректное выражение
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                _calculator.Calculate(expression);
+            });
+        }
+    }
+}
